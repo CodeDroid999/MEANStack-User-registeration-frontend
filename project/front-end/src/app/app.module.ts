3d93c754890d50a9ff67ee20a88bc75ea03c8e9c
CodeDroid999 <johnmwega2002@gmail.com>
2023-01-05 19:43:54 +0300

    generate user component

--- project/front-end/src/app/app.module.ts
+++ project/front-end/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
+import { SignupComponent } from './user/signup/signup.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserComponent
+    UserComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
