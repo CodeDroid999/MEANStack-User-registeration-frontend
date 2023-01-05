8d969832c92cc97146e5553f93ab85e0da277939
CodeDroid999 <johnmwega2002@gmail.com>
2023-01-05 19:51:09 +0300

    add sign-up component

--- project/front-end/src/app/app.module.ts
+++ project/front-end/src/app/app.module.ts
@@ -4,13 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
-import { SignupComponent } from './user/signup/signup.component';
+import { SignupComponent, SignUpComponent } from './user/sign-up/sign-up.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UserComponent,
-    SignupComponent
+    SignupComponent,
+    SignUpComponent
   ],
   imports: [
     BrowserModule,
--- project/front-end/src/app/user/sign-up/sign-up.component.spec.ts
+++ project/front-end/src/app/user/sign-up/sign-up.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { SignupComponent } from './signup.component';
+import { SignUpComponent } from './sign-up.component';
 
-describe('SignupComponent', () => {
-  let component: SignupComponent;
-  let fixture: ComponentFixture<SignupComponent>;
+describe('SignUpComponent', () => {
+  let component: SignUpComponent;
+  let fixture: ComponentFixture<SignUpComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ SignupComponent ]
+      declarations: [ SignUpComponent ]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(SignupComponent);
+    fixture = TestBed.createComponent(SignUpComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
